8470bb99ec3f2b81cfb48ea23ea87ae65c0cfa32
SirDWalker <samuelashby07@icloud.com>
2025-11-09 18:06:09 +0000

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sirdwalker-portfolio/', // important for GitHub Pages
+  base: '/sirdwalker-portfolio/',
 })
 
